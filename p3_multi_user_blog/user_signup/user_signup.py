63ae7821fc7ac076008e7637891fdd3919a0f818
Courtney Noonan <courtneynoonan@me.com>
2016-12-15 11:23:50 -0500

    adding app.yaml for GAE and defining input validation

--- p3_multi_user_blog/user_signup/user_signup.py
+++ p3_multi_user_blog/user_signup/user_signup.py
@@ -1,16 +1,74 @@
 import os
 import webapp2
-import jinja2
+import re  # imports regex module
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
-                               autoescape=True)
+# regex constant for username validation
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+# regex constant for password validation
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+# regex constant for email validation
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+# signup form for main page
+signup_form = """
+    <h1>Signup</h1>
+    <br>
+    <form method="post">
+        <label>Name
+            <input type="text" name="username">
+        </label>
+            <br>
+        <label>Password
+            <input type="password" name="password">
+        </label>
+            <br>
+        <label>Confirm
+            <input type="password" name="verify">
+        </label>
+            <br>
+        <label>Email (optional)
+            <input type="text" name="email">
+        </label>
+        <br>
+        <br>
+        <input type="submit" name="submit">
+"""
 
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    def render_str(self, temple, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
+    # def render_str(self, temple, **params):
+    #     t = jinja_env.get_template(template)
+    #     return t.render(params)
+
+
+class MainPage(Handler):
+    def get(self):
+        self.write(signup_form)
+
+    def post(self):
+        user_username = valid_username(self.request.get('username'))
+        user_password = valid_password(self.request.get('password'))
+        user_email = valid_email(self.request.get('email'))
+        self.write("Thanks for signing up!")
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
+
+
+# validates users username against username regex
+def valid_username(username):
+    return USER_RE.match(username)
+
+
+# validates users password against password regex
+def valid_password(password):
+    return PASSWORD_RE.match(password)
+
+
+# validates users email address against email regex
+def valid_email(email):
+    return EMAIL_RE.match(email)
