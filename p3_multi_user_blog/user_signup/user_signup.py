324b9fd2dc7d1b35f15bc1723fe8acdabb9708b1
Courtney Noonan <courtneynoonan@me.com>
2017-01-09 12:00:23 -0500

    completing welcome page

--- p3_multi_user_blog/user_signup/user_signup.py
+++ p3_multi_user_blog/user_signup/user_signup.py
@@ -1,37 +1,53 @@
 import os
 import webapp2
+import cgi
 import re  # imports regex module
 
-# regex constant for username validation
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-# regex constant for password validation
-PASSWORD_RE = re.compile(r"^.{3,20}$")
-# regex constant for email validation
-EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
 
 # signup form for main page
 signup_form = """
+    <style>
+        .error {
+            color:red;
+        }
+    </style>
     <h1>Signup</h1>
     <br>
     <form method="post">
-        <label>Name
-            <input type="text" name="username">
-        </label>
+        <table>
+            <tbody>
+                <tr>
+                    <td class="label">Username</td>
+                    <td><input type="text" name="username"
+                        value="%(username)s"></td>
+                    <td class="error">%(error)s</td>
+                </tr>
+                <tr>
+                    <td class="label">Password</td>
+                    <td><input type="password" name="password"
+                        value="%(password)s"></td>
+                    <td class="error"></td>
+                </tr>
+                <tr>
+                    <td class="label">Confirm</td>
+                    <td><input type="password" name="verify"
+                    value="%(verify)s"></td>
+                    <td class="error"></td>
+                </tr>
+                <tr>
+                    <td class="label">Email</td>
+                    <td><input type="text" name="email"
+                    value="%(email)s"></td>
+                    <td class="error"></td>
+            </tbody>
+        </table>
             <br>
-        <label>Password
-            <input type="password" name="password">
-        </label>
-            <br>
-        <label>Confirm
-            <input type="password" name="verify">
-        </label>
-            <br>
-        <label>Email (optional)
-            <input type="text" name="email">
-        </label>
-        <br>
-        <br>
-        <input type="submit" name="submit">
+            <input type="submit" name="submit">
+</form>
+"""
+
+welcome_page = """
+    <h1>Welcome, %(username)s</h1>
 """
 
 
@@ -44,23 +60,12 @@ class Handler(webapp2.RequestHandler):
     #     return t.render(params)
 
 
-class MainPage(Handler):
-    def get(self):
-        self.write(signup_form)
-
-    def post(self):
-        user_username = valid_username(self.request.get('username'))
-        user_password = valid_password(self.request.get('password'))
-        user_email = valid_email(self.request.get('email'))
-
-        if not (user_username and user_password and user_email):
-            self.write(signup_form)
-        else:
-            self.write("Thanks for signing up!")
-
-app = webapp2.WSGIApplication([
-    ('/', MainPage)
-], debug=True)
+# regex constant for username validation
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+# regex constant for password validation
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+# regex constant for email validation
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
 
 
 # validates users username against username regex
@@ -72,7 +77,60 @@ def valid_username(username):
 def valid_password(password):
     return PASSWORD_RE.match(password)
 
+# validates users password against users verify  TODO - write function
+# def valid_verify(verify):
+#     if user_password == user_verify:
+#         return
+
 
 # validates users email address against email regex
 def valid_email(email):
     return EMAIL_RE.match(email)
+
+
+def escape_html(string):
+    return cgi.escape(string, quote=True)
+
+
+class MainPage(Handler):
+    def write_form(self, error="", username="", password="", verify="",
+                   email=""):
+        self.write(signup_form % {"error": error,
+                                  "username": escape_html(username),
+                                  "password": escape_html(password),
+                                  "verify": escape_html(verify),
+                                  "email": escape_html(email)})
+
+    def get(self):
+        self.write_form()
+
+    def post(self):
+        user_username = self.request.get('username')
+        user_password = self.request.get('password')
+        user_verify = self.request.get('verify')
+        user_email = self.request.get('email')
+
+        username = valid_username(user_username)
+        password = valid_password(user_password)
+        verify = user_verify  # TODO write verification for matching passwords
+        email = valid_email(user_email)
+
+        # checks validity of input TODO add password/verify match
+        if not ((password and verify) and (username or email)):
+            self.write_form("That doesn't look valid!", user_username,
+                            user_email)
+            # TODO keep user email upon form rewrite
+        else:
+            self.redirect("/welcome?username=" + user_username)
+
+
+class WelcomePage(Handler):
+    def get(self):
+        username = self.request.get('username')
+        self.write(welcome_page % {"username": username})  # TODO sort username variable
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/welcome', WelcomePage)
+], debug=True)
