281a9e92effe91ca8e964e67f006457fe1a1d045
Courtney Noonan <courtneynoonan@me.com>
2017-01-16 13:07:00 -0500

    basic signup functionality

--- p3_multi_user_blog/basic_blog_homework/basic_blog.py
+++ p3_multi_user_blog/basic_blog_homework/basic_blog.py
@@ -1,4 +1,6 @@
 import os
+import cgi
+import re
 import webapp2
 import jinja2
 
@@ -80,6 +82,65 @@ class SignUp(Handler):
 
     def get(self):
         self.render('signup.html')
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        params = dict(username=username, email=email)
+
+        if not valid_password(username):
+            params['error_username'] = "Username is not valid"
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "Password is not valid"
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Password did not match"
+            have_error = True
+
+        if email and not valid_email(email):
+            params['error_email'] = "Email is not valid"
+            have_error = True
+
+        if have_error:
+            self.render('signup.html', **params)
+
+
+
+# SIGN UP PAGE FUNCTIONS
+
+# regex constant for username validation
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+# regex constant for password validation
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+# regex constant for email validation
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+
+# validates users username against username regex
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+
+# validates users password against password regex
+def valid_password(password):
+    return password and PASSWORD_RE.match(password)
+
+
+# validates users email address against email regex
+def valid_email(email):
+    return email and EMAIL_RE.match(email)
+
+
+def escape_html(string):
+    return cgi.escape(string, quote=True)
+
+# Page Mapping
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/newpost', NewPost),
